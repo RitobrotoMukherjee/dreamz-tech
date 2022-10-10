845abb3b97c780184c7db833a457978e19f9adbe
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-10-10 13:30:14 +0530

    Add request handler for add taxes

---
+++ backend/middlewares/route_middleware.js
@@ -0,0 +1,32 @@
+const { ERROR_RESPONSE } = require('../utils/response_helper');
+
+/**
+ * 
+ * @param {http request} req 
+ * @param {http response} resp 
+ * @param {function} next 
+ * 
+ * Checks if an array of object is sent 
+ */
+
+const CheckMultipleData = (req, resp, next) => {
+    try{
+        const { taxes } = req.body;
+
+        if(!taxes.isArray) {
+            return resp.status(400).send({
+                ...ERROR_RESPONSE, msg: 'Bad Request',
+                details: 'Array of taxes required in request body'
+            });
+        }
+        next();
+
+    }catch(e) {
+        return resp.status(400).send({
+            ...ERROR_RESPONSE, msg: 'Bad Request',
+            details: 'POST request body not sent properly, please check your request.'
+        });
+    }
+}
+
+module.exports = { CheckMultipleData };
